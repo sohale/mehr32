1ac1c79e7ee7e3e81ff44ccbd2e48a8e6243a894
Sohail Siadatnejad <sohale@gmail.com>
2022-05-17 19:16:26 +0100

    rudimentary build script

---
+++ reconstruction/scripts/build.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eu
+#set -eux # debug
+
+REPOROOT=$(git rev-parse --show-toplevel)
+
+# export PS1="docker>"
+
+# docker run -it --rm --name conangcc11 -v $REPOROOT:/mehr32 conanio/gcc11-ubuntu16.04 /bin/bash
+
+docker run -it --rm --name conangcc11 -v $REPOROOT:/mehr32 conanio/gcc11-ubuntu16.04 /bin/bash -c \
+   -c " \
+      cd /mehr32/reconstruction ;
+      gcc mehr32-as-in-thesis-report.cpp ;
+      bash
+      "
